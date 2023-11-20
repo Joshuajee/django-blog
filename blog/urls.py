666e3f6a7eeea1ea2dbcfb4606e20067d66272b8
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2023-11-20 14:39:37 +0100

    My Django Blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("latest/", views.latest),
+    path("top/", views.top),
+    path("post/<slug:slug>", views.post)
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import data
+import operator
+
 
 # Create your views here.
 
 def index(req):
-    return render(req, "blog/index.html")
+    return render(req, "blog/index.html", {
+        "posts": data.post_data
+    })
 
-def lastest(req):
-    return HttpResponse("Working Fine")
-
+def latest(req):
+    lastest_post = sorted(data.post_data, key=operator.itemgetter('date'), reverse=True)
+    return render(req, "blog/index.html", {
+        'posts': lastest_post
+    })
 def top(req):
-    return HttpResponse("Working Fine")
+    top_post = sorted(data.post_data, key=operator.itemgetter('views'), reverse=True)
+    return render(req, "blog/index.html", {
+        'posts': top_post
+    })
+
+
+def post(req, slug):
+    
+    current_post = [x for x in data.post_data if x['slug'] == slug] 
+    
+    return  render(req, "blog/post.html", {
+        "post": current_post[0] 
+    })
